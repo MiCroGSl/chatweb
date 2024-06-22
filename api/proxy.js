7b60036f1f394d032ab1a8176355e7655410e9b3
MiCroGSl <144621924+MiCroGSl@users.noreply.github.com>
2024-06-23 00:21:57 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://piped.kavin.rocks/";//your website url
+  let target = "http://cyber.alchosting.xyz/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
